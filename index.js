06d74a76b621f89248a8ae4ffbbda31f9bf73490
LeviAlbarran <levianderson.laal@gmail.com>
2016-04-26 23:50:35 -0430

    prueba

--- index.js
+++ index.js
@@ -12,6 +12,8 @@ var app = express();
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
 
+app.set('port', (process.env.PORT || 5000));
+
 app.use(function(req, res, next){
     res.header('Access-Control-Allow-Origin', '*');
   //  res.header('Access-Control-Allow-Headers', 'X-Requested-With');
@@ -19,6 +21,7 @@ app.use(function(req, res, next){
     res.header('Access-Control-Allow-Headers', 'Content-Type');
     next();
 })
+
 
 app.get('/', function(request, response) {
   response.render('pages/index');
@@ -30,5 +33,28 @@ app.use('/api', require('./routes/api'));
 
 
 // Start server
-app.listen(3000);
-console.log('API is running on port 3000');
+app.listen(app.get('port'), function() {
+  console.log('Node app is running on port', app.get('port'));
+});
+
+/* var express = require('express');
+var app = express();
+
+app.set('port', (process.env.PORT || 5000));
+
+app.use(express.static(__dirname + '/public'));
+
+// views is directory for all template files
+app.set('views', __dirname + '/views');
+app.set('view engine', 'ejs');
+
+app.get('/', function(request, response) {
+  response.render('pages/index');
+});
+
+app.listen(app.get('port'), function() {
+  console.log('Node app is running on port', app.get('port'));
+});
+
+
+*/
