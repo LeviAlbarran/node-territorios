9cc135684230284fd5e13825bfe8ad24067c35c0
LeviAlbarran <levianderson.laal@gmail.com>
2016-08-27 12:13:04 -0430

    actualizacion

--- models/usuarios.js
+++ models/usuarios.js
@@ -3,12 +3,13 @@ var mongoose = restful.mongoose;
 
 var usuariosSchema = new mongoose.Schema({
 	id: Number,
+	id_congregacion: Number,
 	nombre: String,
 	correo: String,
 	clave: String,
-	privilegio: String,
 	lat: Number,
 	lng: Number,
-	nivel: Number
+	nivel: Number,
+	activacion: Number
 });
 module.exports = restful.model('usuarios', usuariosSchema);
